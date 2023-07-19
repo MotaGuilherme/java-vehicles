3155696fb049529cb814000a463b1d9bd2fa4692
MotaGuilherme <metatronmota@gmail.com>
2023-07-18 21:18:13 -0300

    feat: error handlings

---
+++ src/main/java/com/guilherme/vehicles/services/exceptions/NullPointerException.java
@@ -0,0 +1,9 @@
+package com.guilherme.vehicles.services.exceptions;
+
+public class NullPointerException extends RuntimeException {
+
+    public NullPointerException(String msg) {
+        super(msg);
+    }
+
+}
--- src/main/java/com/guilherme/vehicles/services/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/guilherme/vehicles/services/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.Instant;
+
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
@@ -14,6 +16,28 @@ public class ResourceExceptionHandler {
 
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(status).body(err);
+
+    }
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<StandardError> dataIntegrity(NullPointerException e, HttpServletRequest request) {
+
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
 }
