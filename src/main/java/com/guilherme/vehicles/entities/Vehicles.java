58ef901c87740e36ce8e809821af09b57d61839e
MotaGuilherme <metatronmota@gmail.com>
2023-07-18 21:18:58 -0300

    feat: vehicle crud

--- src/main/java/com/guilherme/vehicles/entities/Vehicles.java
+++ src/main/java/com/guilherme/vehicles/entities/Vehicles.java
@@ -1,42 +1,40 @@
 package com.guilherme.vehicles.entities;
 
+
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "tb_vehicles")
+@EqualsAndHashCode(of = "id")
 public class Vehicles {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id_vehicle;
     private String name;
     private String brand;
     private String model;
-    private Integer price;
-
-//    @Column(columnDefinition = "BYTEA")
-//    private Byte imgCar;
+    private Double price;
 
     public Vehicles() {
 
     }
 
-    public Vehicles(Long id, String name, String brand, String model, Integer price) {
-        this.id = id;
+    public Vehicles(Long id_vehicle, String name, String brand, String model, Double price) {
+        this.id_vehicle = id_vehicle;
         this.name = name;
         this.brand = brand;
         this.model = model;
         this.price = price;
-
-       // this.imgCar = imgCar;
     }
 
     public Long getId() {
-        return id;
+        return id_vehicle;
     }
 
     public void setId(Long id) {
-        this.id = id;
+        this.id_vehicle = id;
     }
 
     public String getName() {
@@ -63,33 +61,12 @@ public class Vehicles {
         this.model = model;
     }
 
-    public Integer getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(Integer price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
-//    public Byte getImgCar() {
-//        return imgCar;
-//    }
-//
-//    public void setImgCar(Byte imgCar) {
-//        this.imgCar = imgCar;
-//    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Vehicles vehicles = (Vehicles) o;
-
-        return id.equals(vehicles.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
 }
+
